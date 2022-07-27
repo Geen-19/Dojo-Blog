b926268c5465b961ebaf78bf2e704abc262579dd
Geen-19 <angelinedias53@gmail.com>
2022-07-27 20:55:29 +0530

    First Commit to this repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+ 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50
+  const siteLink = 'http://www.google.com';
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1> {title} </h1>
+        <p>Liked {likes} times</p>
+        <p>{10}</p>
+        <p>{"Geen is the real deal tbh"}</p>
+        <p>{title}</p>
+        <p>{[1,2,3,4,5]}</p>
+        <p>{Math.random()}</p>
+        <p>{"Hello This is geen"}</p>
+        <a href={siteLink}> Google Link</a>
+        
+
+      </div>
     </div>
   );
 }
