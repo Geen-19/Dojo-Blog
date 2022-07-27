3e657748a53a2bbd9e9096ac2d1d50a336e6c28f
Geen-19 <angelinedias53@gmail.com>
2022-07-27 21:10:57 +0530

    Few more components been nested

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import './App.css';
- 
+import Navbar from './Navbar';
+ import Home from './Home';
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50
-  const siteLink = 'http://www.google.com';
-  
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1> {title} </h1>
-        <p>Liked {likes} times</p>
-        <p>{10}</p>
-        <p>{"Geen is the real deal tbh"}</p>
-        <p>{title}</p>
-        <p>{[1,2,3,4,5]}</p>
-        <p>{Math.random()}</p>
-        <p>{"Hello This is geen"}</p>
-        <a href={siteLink}> Google Link</a>
-        
-
+        <Home />
       </div>
     </div>
   );
