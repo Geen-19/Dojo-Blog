da51e75558df7952594b636742c5f958f9c3fd7e
Geen-19 <angelinedias53@gmail.com>
2022-07-28 23:09:52 +0530

    Reading from the lists in JS

---
+++ src/BlogList.js
@@ -0,0 +1,17 @@
+const BlogList = (props) => {
+    const blogs = props.blogs;
+    const title = props.title;
+    console.log(props, blogs);
+    return(
+        <div className="blog-list">
+            {blogs.map((blog) =>  (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{title}</h2>
+                    <h2>{blog.title}</h2>
+                    <p>Written By {blog.author}</p>
+                </div>
+           ))}
+        </div>
+    );
+}
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,19 @@
 import { useState } from "react"
+import BlogList from "./BlogList";
 
 const Home = () => {
-    //let name = 'Mario';
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-    const handleClick = () => {
-        setName('luigi');
-        setAge(45);
-    }
-   
+    const [blogs, setBlogs]  = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
     return (
         //Hooks helps us to make a reactive value; Therefore we use useState
         <div className="home">
-           
-            <h2>Homepage</h2>
-            <p>{ name } is {age} years old </p>
-            <button onClick={handleClick}>Click me</button>
+           <BlogList blogs= {blogs} title = "All Blogs!"/>
         </div>
       );
 }
 
- 
+
 export default Home;
