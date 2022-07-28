c74527b30a262a1273b83f9c77ef1991ab89abb7
Geen-19 <angelinedias53@gmail.com>
2022-07-28 22:32:46 +0530

    Navbar.js and Home.js are modified

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,24 @@
+import { useState } from "react"
+
 const Home = () => {
+    //let name = 'Mario';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+    const handleClick = () => {
+        setName('luigi');
+        setAge(45);
+    }
+   
     return (
+        //Hooks helps us to make a reactive value; Therefore we use useState
         <div className="home">
+           
             <h2>Homepage</h2>
+            <p>{ name } is {age} years old </p>
+            <button onClick={handleClick}>Click me</button>
         </div>
       );
 }
+
  
 export default Home;
