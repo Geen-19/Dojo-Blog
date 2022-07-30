28d3c64426e70d1fb9310387d9f6ea1b1e29bba8
Geen-19 <angelinedias53@gmail.com>
2022-07-30 20:15:14 +0530

    fetching json data

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,15 +1,15 @@
 const BlogList = (props) => {
     const blogs = props.blogs;
     const title = props.title;
-    console.log(props, blogs);
+    const handleDelete = props.handleDelete;
     return(
         <div className="blog-list" key= {blogs.id}>
             <h2>{title} </h2>
             {blogs.map((blog) =>  (
                 <div className="blog-preview" key={blog.id}>
-                    
                     <h2>{blog.title}</h2>
                     <p>Written By {blog.author}</p>
+                   
                 </div>
            ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,27 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs]  = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+    const [blogs, setBlogs]  = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+            .then(Res => {
+                return Res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+            });
+        }, 1000);
+    }, []);
+        //[] this is used in useEffect so that this fun is executed only once when we rerender and not when a state is changed.
     return (
         //Hooks helps us to make a reactive value; Therefore we use useState
         <div className="home">
-           <BlogList blogs= {blogs} title = "All Blogs!"/>
-           <BlogList blogs= {blogs.filter((blog) => blog.author === 'mario')} title = "Mario's Blogs"/>
+            {isPending && <div>Loading...</div>}
+           {blogs && <BlogList blogs= {blogs} title = "All Blogs!"/>}
         </div>
       );
 }
