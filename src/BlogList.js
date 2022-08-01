502fe3aadcf2f3808e3bb664c6736fc149ee5c29
Geen-19 <angelinedias53@gmail.com>
2022-08-01 19:49:45 +0530

    To get the blog dtails whnever a particular blog link(using id) is clicked

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom";
+
 const BlogList = (props) => {
     const blogs = props.blogs;
     const title = props.title;
-    const handleDelete = props.handleDelete;
     return(
         <div className="blog-list" key= {blogs.id}>
             <h2>{title} </h2>
             {blogs.map((blog) =>  (
                 <div className="blog-preview" key={blog.id}>
+                    <Link to = {`/blogs/${blog.id}`}>
                     <h2>{blog.title}</h2>
                     <p>Written By {blog.author}</p>
+                    </Link>
+                    
                    
                 </div>
            ))}
