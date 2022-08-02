a49c14fa8293a86e8469ee61a25299154f975aae
Geen-19 <angelinedias53@gmail.com>
2022-08-02 20:02:45 +0530

    The website is styled using css

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,8 +3,7 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
-            <div className="links">
-                
+            <div className="links"> 
                 <Link to="/">Home</Link>
                 <Link to="/create" >New Blog</Link>
             </div>
