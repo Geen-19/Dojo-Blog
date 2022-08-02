a4781afae935037731b0df27d2148b373c9e3c92
Geen-19 <angelinedias53@gmail.com>
2022-08-02 15:35:15 +0530

    The blogs now can be deleted using a DELETE request

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,29 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
    const[title , setTitle]= useState('');
    const[body , setBody]= useState('');
    const [author, setAuthor] = useState('mario');
+   const [isPending, setIsPending] = useState(false);
+   const history = useHistory();
 
    const handleSubmit = (e) => {
       e.preventDefault();
-      const blog = {title, body, author}
-       console.log(blog);
+      const blog = {title, body, author};
+
+      setIsPending(true);
+
+      fetch('http://localhost:8000/blogs', {
+         method: 'POST',
+         headers: {"Content-Type": "application/json"},
+         body: JSON.stringify(blog)
+      }).then(()=>{
+         console.log("New blog added");
+         setIsPending(false);
+         history.push('/');
+      })
+      
    }
     return (
         <div className="create">
@@ -33,7 +48,8 @@ const Create = () => {
                <option value="mario">mario</option>
                <option value="yoshi">yoshi</option>
             </select>
-            <button>Add blog</button>
+           {!isPending && <button>Add blog</button>}
+           {isPending && <button>Adding Blog...</button>}
             
            </form>
         </div>
